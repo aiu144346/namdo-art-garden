e3a28724fa41d66af6b725d6baaa77356fed3351
aiu144346 <bluerain1026@gmail.com>
2026-07-04 10:05:10 +0900

    Updates

--- assets/index-DMJbEhS1.js
+++ assets/index-DMJbEhS1.js
@@ -4691,7 +4691,7 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
               </p>
             </div>
             <div class="md:w-1/2 w-full">
-              <img src="/images/haenam-189.webp" alt="기하학적인 자연의 아름다움" class="rounded-2xl shadow-md w-full h-56 object-cover" />
+              <img src="/images/haenam-171.webp" alt="기하학적인 자연의 아름다움" class="rounded-2xl shadow-md w-full h-56 object-cover" />
             </div>
           </div>
 
@@ -4912,7 +4912,7 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
               <img src="/images/haenam-220.webp" alt="문가든 미공개 갤러리 29" class="w-full h-40 object-cover group-hover:scale-105 transition duration-500" />
             </div>
             <div class="group relative overflow-hidden rounded-2xl shadow-sm border border-neutral-100 hover:shadow-md transition duration-300">
-              <img src="/images/haenam-179.webp" alt="문가든 미공개 갤러리 30" class="w-full h-40 object-cover group-hover:scale-105 transition duration-500" />
+              <img src="/images/haenam-189.webp" alt="문가든 미공개 갤러리 30" class="w-full h-40 object-cover group-hover:scale-105 transition duration-500" />
             </div>
             <div class="group relative overflow-hidden rounded-2xl shadow-sm border border-neutral-100 hover:shadow-md transition duration-300">
               <img src="/images/haenam-191.webp" alt="문가든 미공개 갤러리 31" class="w-full h-40 object-cover group-hover:scale-105 transition duration-500" />
